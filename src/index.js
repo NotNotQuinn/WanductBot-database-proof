8ae0e708855f1119e078e371f466b96f4112ee39
Quinn T <Quinn.github@gmail.com>
2021-02-16 21:59:44 -0600

    eShrug I did shit

--- src/index.js
+++ src/index.js
@@ -2,8 +2,19 @@ const ircLib = require("dank-twitch-irc");
 const { prefixMatcher } = require("./prefixMatcher")
 const { config, bot, db_conf } = require("./config");
 const mariadb = require("mariadb");
+const _ = require("lodash/isEqual");
+
+const arrayContainsObject = (array, obj) => {
+    for (____obj_in_arr in array) {
+        if (_.isEqual(____obj_in_arr, obj))
+            return true;
+    }
+    return false;
+};
+
 
 let db_pool;
+let users_seen = [];
 
 const client = new ircLib.ChatClient(config);
 
@@ -30,11 +41,28 @@ var createConnectionsInterval = setInterval(() => {
     
     client.newConnection();
     connectionsCreatedOnStart++;
-    console.log(`[META] createConnectionsInterval: Connection created: #${connectionsCreatedOnStart}`);
+    console.log(`[META] New connection: #${connectionsCreatedOnStart}`);
     
     if (--connectionsToCreateOnStart <= 0)
     {clearInterval(createConnectionsInterval);}
-}, 2000);
+}, 5000);
+
+
+
+var addNewUsertoDatabase = setInterval(() => {
+    var default_msg = "[META] New users added to db:";
+    var str = default_msg;
+    for (let i = 0; i < users_seen.length; i++) {
+        if(users_seen[i].name && users_seen[i].id) 
+        {   
+            //console.log(users_seen[i])
+            str += " " + users_seen[i].name + ",";
+        }
+    }
+    if(str.length > default_msg.length)
+        console.log(str.substr(0, str.length-1));
+}, 60000) // 1 min
+
 
 
 
@@ -44,9 +72,19 @@ client.on("ready", () => {
 
 client.on("PRIVMSG", (msg) => {
     
+    console.log(`[#${msg.channelName}] ${msg.displayName}: ${msg.messageText}`);
     if(msg.senderUserID == bot.uid)
         return;
-    console.log(`[#${msg.channelName}] ${msg.displayName}: ${msg.messageText}`);
+    (()=>{
+        let seen = false;
+        for(let i = 0; i < users_seen.length; i++) {
+            if(users_seen[i].id == msg.senderUserID) {seen = true;}
+        }
+        if (!seen) { 
+            users_seen.push({ name: msg.senderUsername, id: msg.senderUserID }) 
+        }
+    })();
+
     
     prefixMatch = prefixMatcher(msg.channelName, msg.messageText)
     msg.mText = msg.messageText.substr(prefixMatch.prefix.length);
@@ -60,8 +98,13 @@ client.on("PRIVMSG", (msg) => {
         if(msg.mArgs[0].toLowerCase() == "ping") {
             client.say(msg.channelName, "🅱ONG!")
         }
+
+        if(msg.mArgs[0].toLowerCase() == "su" || msg.mArgs[0].toLowerCase() == "usersseen") {
+            client.say(msg.channelName, `lol! ive spotted ${users_seen.length} new users since last startup!`)
+        }
+
         if(msg.mArgs[0].toLowerCase() == "echo") {
-            
+            client.privmsg(msg.channelName, msg.mArgs.slice(1).join(" "))
         }
         
         if(msg.mArgs[0].toLowerCase() == "spam")
@@ -80,7 +123,7 @@ client.on("PRIVMSG", (msg) => {
                 }
                 
                 for(let i=0; i< parseInt(msg.mArgs[1]);i++) {
-                    var messageToSay = msg.mArgs.slice(2, msg.mArgs.length).join(" ")
+                    var messageToSay = msg.mArgs.slice(2).join(" ")
                     if(messageToSay.length <= 0)
                     {
                         client.privmsg(msg.channelName, "Spam what WholeWheat");
@@ -101,13 +144,14 @@ client.on("PRIVMSG", (msg) => {
 
 
 client.on("error", (err) => {
-    console.error("Error?", err);
+    console.error("Unhandled error in bot. ", err);
 });
 
 
 client.use(new ircLib.AlternateMessageModifier(client));
 
 
-
-client.connect();
-client.joinAll(bot.channelsToConnect);
+setTimeout(() => {
+    client.connect();
+    client.joinAll(bot.channelsToConnect);
+}, 1000);
